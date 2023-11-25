128313829016cd239e50c0af9ecc1c2f2046c223
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-11-24 22:58:59 -0300

    Mudança de textos

--- pages/3_⭐_Motivação.py
+++ pages/3_⭐_Motivação.py
@@ -9,14 +9,13 @@ with open("styles.css") as f:
 
     st.title("Motivação")
 
-    # Carregar as imagens
     kaiky = Image.open("imagens/kaiky.jpg")
     samuel = Image.open("imagens/samuel.jpg")
 
     # Tamanho desejado para as imagens redondas
     tamanho_redondo = (2000, 2000)
 
-    # Criar máscaras circulares
+    # Criaçaõ de máscaras circulares
     def aplicar_mascara(imagem, mascara):
         imagem = ImageOps.fit(imagem, mascara.size, centering=(0.5, 0.5))
         imagem.putalpha(mascara)
@@ -29,11 +28,11 @@ with open("styles.css") as f:
     kaiky = aplicar_mascara(kaiky, mascara)
     samuel = aplicar_mascara(samuel, mascara)
 
-    # Adicionar margens transparentes às imagens para criar espaço entre elas
+    # Margens transparentes às imagens para criar espaço entre elas
     kaiky = ImageOps.expand(kaiky, border=30, fill=None)
     samuel = ImageOps.expand(samuel, border=30, fill=None)
 
-    # Exibir imagens redondas lado a lado com espaçamento e nomes abaixo delas
+    # Imagens redondas lado a lado com espaçamento e nomes abaixo delas
     st.image([kaiky, samuel], width=350, caption=["Kaiky Braga", "Samuel Lima"])
 
     st.write("Olá! Nós, estudantes do 2º Período de Ciência de Dados da Escola de Matemática Aplicada da Fundação Getúlio Vargas, estamos entusiasmados em compartilhar nosso projeto, no qual buscamos visualizar de maneira gráfica as estruturas de treinos de musculação utilizando Grafos. Essa abordagem é aprimorada com a interatividade proporcionada pela biblioteca Streamlit. Estamos ansiosos para apresentar os resultados desta iniciativa que integra a precisão dos dados com a dinâmica da visualização, proporcionando uma experiência informativa e envolvente.")
